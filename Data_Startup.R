845e8996ff425df68224bebf7fb485bac83c871f
IgnacioZQ <70081566+IgnacioZQ@users.noreply.github.com>
2020-11-28 11:04:43 -0300

    UPDATE_Data_Startup

---
+++ Data_Startup.R
@@ -0,0 +1,16 @@
+# Observaciones
+
+# Que tipo de startups tiene mas exito.
+
+# Que tipo de Startups ya no se pueden sustentar.
+
+# Cuales son las Startups mas estables.
+
+# Datos a trabajar: Año de la inversión, Inversiones, Categoria de la inversión (Company_Category_Code).
+
+# Pregunta / Observación 1: Cual es la categoria de Startup con mayores inversiones a lo largo de los años (Gráfico de Linea)
+
+# Pregunta / Observación 2: Ver el total e inversión en cada categoria y comparar cual ha recibido mayor inversión en el tiempo (Gráfico de Barra / Columna).
+
+View(crunchbase_investments)
+
