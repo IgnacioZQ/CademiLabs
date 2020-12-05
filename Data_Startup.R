26f20592e44a4aba2223bf831f0be03ff9a7587d
IgnacioZQ <Ignacio.zamora@mail.udp.cl>
2020-12-05 20:07:30 -0300

    Update Data_Startup.R

--- Data_Startup.R
+++ Data_Startup.R
@@ -1,6 +1,9 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
+library(forcats)
+library(countrycode)
 
 # Observaciones
 
@@ -28,13 +31,29 @@ Dataset <- select(crunchbase_investments, company_category_code, funded_year, ra
 
 Data_Limpia <- read_csv("crunch2013/Data_Limpia.csv")
 
-#Filraremos solamente las inversiones sobre 10.000.000 USD desde el año 2000 hasta 2013
+#Filraremos solamente las inversiones sobre 10.000.000 USD desde el año 1995 hasta 2013
 
 Data_Limpia_2 <- filter(Data_Limpia, Data_Limpia$raised_amount_usd >= 10000000,
-                        Data_Limpia$funded_year >= 2000)
+                        Data_Limpia$funded_year >= 1995)
 Data_Limpia_2 <- na.omit(Data_Limpia_2) # (Eliminar NA y valores nulos)
 
 # IDEA: Podriamos segmentar las variables en varios tipos.
 # Ej: ECONOMY (ecommerce, enterpise, finance) TECHNOLOGY (analytics, game_video, mobile, nanotech, software)
 
 ### Grafica
+
+ggplot(data = Data_Limpia_2, aes(x = company_category_code, y = raised_amount_usd)) +
+  geom_segment(aes(x = company_category_code, xend = company_category_code, y = 0, yend = raised_amount_usd)) +
+  geom_point(size=0.2, color= "orange", alpha=0.5, shape=21, stroke=2) +
+  theme_light() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  coord_flip() +
+  labs(title = paste("Movimientos de Inversiones a Startups entre 1995 - 2013 a partir de los 10.000.000 USD"), 
+       caption = "FUENTE: Base de datos Crunchbase", 
+       y = "Inversion en USD", 
+       x = ""
+  )
