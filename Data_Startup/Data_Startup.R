daa309665fc95cbfe8bb6ee94c20799be66bc4a0
IgnacioZQ <Ignacio.zamora@mail.udp.cl>
2020-12-21 01:04:26 -0300

    Update Data_Startup.R

--- Data_Startup/Data_Startup.R
+++ Data_Startup/Data_Startup.R
@@ -28,9 +28,11 @@ Data_Limpia <- read.csv(text = Data_Limpia)
 
 #Filraremos solamente las inversiones sobre 10.000.000 USD desde el año 1995 hasta 2013
 
-Data_Limpia_2 <- filter(Data_Limpia, Data_Limpia$raised_amount_usd >= 10000000,
-                        Data_Limpia$funded_year >= 1995)
+Data_Limpia_2 <- filter(Data_Limpia, Data_Limpia$raised_amount_usd >= 10000000, Data_Limpia$funded_year >= 1995)
+
 Data_Limpia_2 <- na.omit(Data_Limpia_2) # (Eliminar NA y valores nulos)
+
+Data_Limpia_3 <- select(Data_Limpia_2, company_category_code, funded_year, raised_amount_usd)
 
 # IDEA: Podriamos segmentar las variables en varios tipos.
 # Ej: ECONOMY (ecommerce, enterpise, finance) TECHNOLOGY (analytics, game_video, mobile, nanotech, software)
@@ -41,7 +43,7 @@ Data_Limpia_2 <- na.omit(Data_Limpia_2) # (Eliminar NA y valores nulos)
 
 ##Gráfico 1 (Plantilla)
 
-ggplot(data = Data_Limpia_2, aes(x = company_category_code, y = raised_amount_usd, color = funded_year)) +
+ggplot(data = Data_Limpia_3, aes(x = company_category_code, y = raised_amount_usd, color = funded_year)) +
   geom_segment(aes(x = company_category_code, xend = company_category_code, y = 0, yend = raised_amount_usd)) +
   geom_point(size=0.2, color= "orange", alpha=0.5, shape=21, stroke=2) +
   theme_light() +
@@ -68,7 +70,7 @@ ggplot(data = Data_Limpia_2, aes(x = company_category_code, y = raised_amount_us
 
 ## Gráfico de burbuja 1
 
-ggplot(Data_Limpia_2, mapping = aes(x = funded_year, y = raised_amount_usd, size = raised_amount_usd, colour = raised_amount_usd)) +
+ggplot(Data_Limpia_3, mapping = aes(x = funded_year, y = raised_amount_usd, size = raised_amount_usd, colour = raised_amount_usd)) +
   geom_point(alpha=0.4) +
   scale_size_continuous(range=c(1, 24)) +
   scale_colour_continuous(guide = FALSE) +
@@ -81,12 +83,12 @@ ggplot(Data_Limpia_2, mapping = aes(x = funded_year, y = raised_amount_usd, size
        caption="FUENTE: Base de Datos Crunchbase 2013") +
   scale_y_continuous(labels = scales::dollar_format()) +
   theme (text = element_text(size=10)) +
-  geom_text(data = Data_Limpia_2 %>% filter(raised_amount_usd >= 600000000), aes(x = funded_year, y = raised_amount_usd, label = company_category_code), color="black", fontface="bold",alpha= 0.5, size=3.5, inherit.aes = FALSE)
+  geom_text(data = Data_Limpia_3 %>% filter(raised_amount_usd >= 600000000), aes(x = funded_year, y = raised_amount_usd, label = company_category_code), color="black", fontface="bold",alpha= 0.5, size=3.5, inherit.aes = FALSE)
 
 
 ## Gráfico de burbuja 2 (DEFINITIVO).
 
-ggplot(data = Data_Limpia_2, aes(x = company_category_code, y = funded_year, color = raised_amount_usd, size = raised_amount_usd)) +
+ggplot(data = Data_Limpia_3, aes(x = company_category_code, y = funded_year, color = raised_amount_usd, size = raised_amount_usd)) +
   geom_point(alpha=0.04) +
   theme (text = element_text(size=10)) +
   coord_flip() +
@@ -107,5 +109,45 @@ ggplot(data = Data_Limpia_2, aes(x = company_category_code, y = funded_year, col
 # A partir del 2005 Biotech, Cleantech, Ecommerce, Education, Medical, Advertising,  Mobile, Nanotech, Network_Hosting, Security y software Tvieron un impulso de Inversiones a partir del 2005.
 # Las Satartups que tuvieron menos inversiones y no se mantuvieron con el tiempo fueron Legal, Local, Design, Pets, Sport y Nonprofit.
 
-## Gráfico "Total de Inversiones"
-# Gráfico de Barras.
+## Gráfico "Total de Inversiones por Categoria"
+
+Total_Inv_Cat <- Data_Limpia_3 %>%
+  group_by(company_category_code) %>%
+  summarise(sum(raised_amount_usd))
+
+ggplot(data = Total_Inv_Cat, aes(x = company_category_code, y = `sum(raised_amount_usd)`)) +
+  geom_segment(aes(x = company_category_code, xend = company_category_code, y = `sum(raised_amount_usd)`, yend = 0), size = 1.5, color = "gray") +
+  theme_light() +
+  geom_point( size = 3, color="gray") +
+  coord_flip() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank()
+  ) +
+  labs(title="Total de Inversiones a Startups entre 1995 - 2013 por Categoria a partir de los 10.000.000 USD", 
+       caption="FUENTE: Base de Datos Crunchbase 2013") +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  ylab("Investment in USD") +
+  xlab("Category")
+
+## Gráfico "Total de Inversiones por Año"
+
+Total_Inv_Year <- Data_Limpia_3 %>%
+  group_by(Data_Limpia_3$funded_year) %>%
+  summarise(sum(raised_amount_usd))
+
+ggplot(data = Total_Inv_Year, aes(x = `Data_Limpia_3$funded_year`, y = `sum(raised_amount_usd)`)) +
+  geom_segment(aes(x = `Data_Limpia_3$funded_year`, xend = `Data_Limpia_3$funded_year`, y = `sum(raised_amount_usd)`, yend = 0), size = 8, color = "gray") +
+  theme_light() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank()
+  ) +
+  labs(title="Total de Inversiones a Startups por Año entre 1995 - 2013 a partir de los 10.000.000 USD", 
+       caption="FUENTE: Base de Datos Crunchbase 2013") +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  scale_x_continuous(breaks = c(1995, 1997, 1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013)) +
+  ylab("Investment in USD") +
+  xlab("Year")
